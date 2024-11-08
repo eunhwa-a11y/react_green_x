17e51424f684b530258a137ef95a45e3bb37605c
eunhwa-a11y <ttab136@gmail.com>
2024-11-08 11:52:41 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+import { authService } from '../firebase';
+import AppRouter from './Routes';
+console.log(authService);
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+      <AppRouter isLoggedIn = {isLoggedIn}/>
+  );
+}
+
+export default App;
