0e6bfae6e0f857124cb55308a45c7829094d534e
eunhwa-a11y <ttab136@gmail.com>
2024-11-11 12:02:50 +0900

    유저 정보 전달

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import { db } from "../firebase";
-import { collection, addDoc, serverTimestamp, query, where, getDocs } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp, query, getDocs, orderBy, limit } from "firebase/firestore";
 import ListGroup from 'react-bootstrap/ListGroup'; 
 import Comment from "../components/Comment";
 
-const Home = () => {
+const Home = (userObj) => {
   const [comment, setComment] = useState(''); // 입력하는 글 정보 담기
   const [comments, setComments] = useState([]); // 조회된 글 배열 담기
 
   const getComments = async () => {
 
-    
-    const q = query(collection(db, "comments"));
+    // orderBy("date", "desc") 날짜 순으로 최근 글이 제일 위로 올라오게
+    const q = query(collection(db, "comments"), orderBy("date", "desc"), limit(5)); 
     const querySnapshot = await getDocs(q);
     /*
     setComments가 너무 많이 많이 반복됨, 비효율적
@@ -33,7 +33,6 @@ const Home = () => {
    setComments(commentArr);
 
   }
-  console.log(comments);
 
   useEffect(() => {
     getComments();
@@ -52,7 +51,8 @@ const Home = () => {
       const docRef = await addDoc(collection(db, "comments"), { // 글 정보가 doRef 변수에 담기고
         // comment,
         comment:comment,
-        date:serverTimestamp()
+        date:serverTimestamp(),
+        uid:userObj
       });
       console.log("Document written with ID: ", docRef.id); // 그 값을 콘솔에 출력
     } catch (e) {
@@ -72,7 +72,7 @@ const Home = () => {
       <hr />
       <ListGroup>
       {comments.map(item=> 
-          <Comment commentObj={item}/>
+          <Comment key={item.id} commentObj={item} isOwener={item.uid === userObj}/>
         )}
       </ListGroup>
     </div>
